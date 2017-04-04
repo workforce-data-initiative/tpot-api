c3a86b13773030b3cd015f31a51e385a6ee1cd61
Matt Bauman <mbauman@gmail.com>
2017-04-04 18:37:39 -0500

    Register the blueprint after initializing DB

--- etp_api/__init__.py
+++ etp_api/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from .apiv0 import blueprint as apiv0
 from flask_sqlalchemy import SQLAlchemy
 from flask.ext.heroku import Heroku
 
 app = Flask(__name__)
-app.register_blueprint(apiv0)
 
 heroku = Heroku(app)
 db = SQLAlchemy(app)
+
+from .apiv0 import blueprint as apiv0
+app.register_blueprint(apiv0)
--- etp_api/database/model.py
+++ etp_api/database/model.py
@@ -1,7 +1,6 @@
 import itertools
 from etp_api import db
 
-
 def get_all_providers():
     r = db.engine.execute("SELECT DISTINCT provider_id FROM scorecard")
     vals = r.fetchall()
@@ -18,6 +17,6 @@ def get_programs_for_provider(provider_id):
     return list(itertools.chain(*vals))
 
 def get_outcomes_for_program(provider_id, program_id):
-    r = db.engine.execute("* FROM scorecard WHERE provider_id=%d and program_cip=program_id" % (provider_id, program_id))
+    r = db.engine.execute("SELECT * FROM scorecard WHERE provider_id=%d and program_cip=program_id" % (provider_id, program_id))
     vals = r.fetchall()
     return list(map(dict, vals))
