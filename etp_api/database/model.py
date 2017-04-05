977ad193eb469f4ef72e53978ecac077f21ebcf2
Matt Bauman <mbauman@gmail.com>
2017-04-04 19:14:24 -0500

    fixup

--- etp_api/database/model.py
+++ etp_api/database/model.py
@@ -17,6 +17,6 @@ def get_programs_for_provider(provider_id):
     return list(map(dict, vals))
 
 def get_outcomes_for_program(provider_id, program_id):
-    r = db.engine.execute("SELECT * FROM scorecard WHERE provider_id=%d and program_cip=program_id" % (provider_id, program_id))
+    r = db.engine.execute("SELECT * FROM scorecard WHERE provider_id=%d and program_cip=%d" % (provider_id, program_id))
     vals = r.fetchall()
     return list(map(dict, vals))
